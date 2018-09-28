aab49a048601b4372b8e5ae1908986e22e56da7c
Husne Dereli <husnedereli@Husnes-MacBook-Pro.local>
2018-09-28 16:03:25 +0200

    adding file

--- accretion_disk.c
+++ accretion_disk.c
@@ -8,6 +8,8 @@
 /** Definition of the physical constants **/
 #define pc 3.08567758e13; /** km **/
 #define G 4.302e-3; /** pc M_sun^-1 (km/s)^-1 **/
+#define c 3e5 /** km/s **/
+
 
 int main()
 {
@@ -16,13 +18,24 @@ int main()
     r = (double *) calloc(Nr,sizeof(double));
     theta = (double *) calloc(Ntheta,sizeof(double));
     
-    double G = 4.302;
-    double M = 20;
-    double c = 10;
-    double Rg= (G*M)/(c*c);
-    double r_min= 6*Rg;
+    
+    double M = 3.2e7; /** M_sun, the black hole mass **/
+    double Rg= (G*pc*M)/(c*c);
+    double r_min= 6 * Rg;
     double r_max=1000;
     
+    
+    double step = exp(log(r_max/r_min)/Nr);
+    int i;
+    for (i=0; i < Nr; i++){
+        r[i] = r_min*pw(step,i);
+    }
+    
+    int i;
+    for (i=0; i < Ntheta; i++){
+        theta[i] = i*(360/100);
+    }
+
     
     free(r);
     free(theta);
