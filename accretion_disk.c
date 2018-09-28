6e6a0b439fcb7290fbcf78f5dced90da1dfc0d72
Husne Dereli <husnedereli@Husnes-MacBook-Pro.local>
2018-09-28 15:11:34 +0200

    adding file

---
+++ accretion_disk.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define Nr 66
+#define Ntheta 66
+
+/* Definition of the physical constants */
+#define pc 3.08567758e13; /* km */
+
+int main()
+{
+    double *r; /* radius */
+    double *theta; /* azimuth angle */
+    r = (double *) calloc(Nr,sizeof(double));
+    theta = (double *) calloc(Ntheta,sizeof(double));
+    
+    double G = 4.302;
+    double M = 20;
+    double c = 10;
+    double Rg= (G*M)/(c*c);
+    double r_min= 6*Rg;
+    double r_max=1000;
+    
+    
+    free(r);
+    free(theta);
+    return 0;
+}
