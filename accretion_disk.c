63079521f272cafa13fd78fef2468d036933ba5b
Husne Dereli <husnedereli@Husnes-MacBook-Pro.local>
2018-10-01 10:27:37 +0200

    adding file

--- accretion_disk.c
+++ accretion_disk.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 
-#define Nr 66
-#define Ntheta 66
+#define Nr 66 /** layers **/
+#define Ntheta 66 /** zones per layers **/
 
 /** Definition of the physical constants **/
-#define pc 3.08567758e13; /** km **/
-#define G 4.302e-3; /** pc M_sun^-1 (km/s)^-1 **/
+#define pc 3.08567758e13 /** km **/
+#define G 4.302e-3 /** pc M_sun^-1 (km/s)^-1 **/
 #define c 3e5 /** km/s **/
 
 
@@ -20,18 +21,17 @@ int main()
     
     
     double M = 3.2e7; /** M_sun, the black hole mass **/
-    double Rg= (G*pc*M)/(c*c);
-    double r_min= 6 * Rg;
-    double r_max=1000;
+    double Rg= (G*pc*M)/(c*c); /** gravitational radius **/
+    double r_in= 6 * Rg; /** inner radius **/
+    double r_out=1000; /** outer radius **/
     
     
-    double step = exp(log(r_max/r_min)/Nr);
+    double step = exp(log(r_out/r_in)/Nr);
     int i;
     for (i=0; i < Nr; i++){
-        r[i] = r_min*pw(step,i);
+        r[i] = r_in*pow(step,i);
     }
     
-    int i;
     for (i=0; i < Ntheta; i++){
         theta[i] = i*(360/Ntheta);
     }
