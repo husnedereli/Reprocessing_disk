66f92d4c3a4c8c9cc6a3a07679a98aaf27746c1c
cayley38 <cayley38@gmail.com>
2018-10-01 13:20:49 +0200

    Correction function

--- accretion_disk.c
+++ accretion_disk.c
@@ -13,18 +13,32 @@
 #define sigma = 5.670373e-8 /** W m^-2 K^4 the Stefan–Boltzmann constant  **/
 #define pi 3.14
 
-/** Function of the temperature profile **/
-int temp_profile (int t, int r, int theta );
-/** Function of the r_star **/
-int r_star (int r);
-/** Function of the lag **/
-int lag_tao (int r, int theta, int inc_angle);
+
+
+double temp_profile (int t, double r, double theta ){
+    return ((3.0*G*M*M_rate)/(8.0*pi*sigma*pow(r,3.0)))*(1.0-sqrt(r_in/r))+(1.0-A)*((h_star*L_star(t-tao_light_travel_time(r, theta, int i)))/(4.0*pi*sigma*pow(r_star,3.0)));
+}
+    
+/** the distance from the cetral variable source to disk elements **/
+int r_star (int r){
+    sqrt(h_star**2+r**2)
+}
+    
+    
+/** the lag **/
+double inc_angle = 45; /** inclination angle **/
+int lag_tao (int r, int theta, int inc_angle){
+   sqrt(h_star**2+r**2)+h_star*cos(inc_angle)-r*cos(theta)*sin(inc_angle)
+}
+    
+
+
 
 
 int main()
 {
-    double *r; /** radius **/
-    double *theta; /** azimuth angle **/
+    double *r; 				/** radius **/
+    double *theta; 			/** azimuth angle **/
     r = (double *) calloc(Nr,sizeof(double));
     theta = (double *) calloc(Ntheta,sizeof(double));
     
@@ -54,23 +68,6 @@ int main()
     double h_star = 10*Rg; /** the vertical distance from the cetral variable source to disk **/
     double L_bol = 2.82e44; /** the bolometric luminosity **/
     double L_star = 0.5*L_bol; /** the luminosity of central variable source **/
-    
-    int temp_profile (int t, int r, int theta ){
-        ((3*G*M*M_rate)/(8*pi*sigma*r**3))*(1-sqrt(r_in/r))+(1-A)*((h_star*L_star(t-tao_light_travel_time(int r, int theta, int i)))/(4*pi*sigma*r_star**3))
-    }
-    
-    /** the distance from the cetral variable source to disk elements **/
-    int r_star (int r){
-        sqrt(h_star**2+r**2)
-    }
-    
-    
-    /** the lag **/
-    double inc_angle = 45; /** inclination angle **/
-    int lag_tao (int r, int theta, int inc_angle){
-        sqrt(h_star**2+r**2)+h_star*cos(inc_angle)-r*cos(theta)*sin(inc_angle)
-
-    }
     
     
     
