19674b236574cb3aec15138a853b65c456e2d099
Husne Dereli <husnedereli@Husnes-MacBook-Pro.local>
2018-11-08 15:30:02 +0100

    add file

---
+++ accretion_disk.h
@@ -0,0 +1,9 @@
+#ifndef ACCRETION_DISK_h
+#define ACCRETION_DISK_h
+
+
+//int rescal_filters(char *filter, int binningnumber);
+
+int make_computation(int Nfilter, long int *computed_filter);
+
+#endif
