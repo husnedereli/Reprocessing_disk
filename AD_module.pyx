cf87f4f358c073534834bc188f1d4b75981136bb
Husne Dereli <husnedereli@Husnes-MacBook-Pro.local>
2018-11-27 11:26:15 +0100

    changes

--- AD_module.pyx
+++ AD_module.pyx
@@ -5,7 +5,7 @@ import ctypes
 np.import_array()
 
 cdef extern from "accretion_disk.h":
-    int make_computation(int Nfilter, long int *computed_filter, long double *time, long double *flux)
+    int make_computation(int Nfilter, long int *computed_filter, double *time, double *flux)
 
 # create the wrapper code, with numpy type annotations
 #def Rescal_filters_py( binningnumber, the string):
@@ -14,6 +14,6 @@ cdef extern from "accretion_disk.h":
 
 
 # create the wrapper code, with numpy type annotations
-def AD_py( Nfilter, np.ndarray[long int, ndim=1, mode="c"] computed_filter not None, np.ndarray[long int, ndim=1, mode="c"] time not None, np.ndarray[long int, ndim=1, mode="c"] flux not None):
+def AD_py( Nfilter, np.ndarray[long int, ndim=1, mode="c"] computed_filter not None, np.ndarray[double, ndim=1, mode="c"] time not None, np.ndarray[double, ndim=1, mode="c"] flux not None):
     #cf = np.ascontiguousarray(computed_filter, dtype=ctypes.c_int)
-    return make_computation(Nfilter, <long int*> np.PyArray_DATA(computed_filter), <long int*> np.PyArray_DATA(time), <long int*> np.PyArray_DATA(flux))
+    return make_computation(Nfilter, <long int*> np.PyArray_DATA(computed_filter), <double*> np.PyArray_DATA(time), <double*> np.PyArray_DATA(flux))
